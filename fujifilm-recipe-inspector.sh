af391f2c16b53c7ba1049209b7a56dc34015c032
Oscar Rundh <1194795+shadovo@users.noreply.github.com>
2025-12-26 17:15:43 +0100

    better bw formatting

--- fujifilm-recipe-inspector.sh
+++ fujifilm-recipe-inspector.sh
@@ -10,8 +10,19 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+if [ ! -f "$1" ]; then
+	echo "Error: File '$1' not found or is not a regular file."
+	exit 1
+fi
+
 file_path="$1"
 file_name="${file_path##*/}"
+
+ansi_invert='\e[1;7m'
+ansi_reset='\e[0m'
+
+box_width=60
+box_label_width=25
 
 all_data=$(
 	exiftool -s \
@@ -27,26 +38,89 @@ all_data=$(
 		-FocalLength \
 		\
 		-FilmMode -HighlightTone -ShadowTone -WhiteBalance -WhiteBalanceFineTune -Saturation \
-		-Sharpness -NoiseReduction -DynamicRange -GrainEffectRoughness -GrainEffectSize \
-		-ColorChromeEffect -ColorChromeFXBlue -Clarity \
+		-Sharpness -NoiseReduction -DynamicRangeSetting -DevelopmentDynamicRange -GrainEffectRoughness -GrainEffectSize \
+		-ColorChromeEffect -ColorChromeFXBlue -Clarity -ColorTemperature \
 		"$file_path" |
 		sed -E "s/\(((very|medium) )?(((soft|high|hard|weak)(est)?)|normal)\)//g"
 )
-function get_value() {
+get_value() {
 	echo "$all_data" |
 		grep -m 1 "^$1" |
 		cut -d ':' -f 2- |
 		xargs
 }
-function format_wb_fine_tune_scaled() {
+
+get_film_sim() {
+	local film_sim
+	film_sim="$(get_value "FilmMode")"
+	test -z "$film_sim" && film_sim=$(get_value "Saturation")
+
+	case "$film_sim" in
+	"F0/Standard (Provia)" | "F1/Studio Portrait" | "F1c/Studio Portrait Increased Sharpness")
+		echo "Provia"
+		;;
+	"F1a/Studio Portrait Enhanced Saturation" | "F1b/Studio Portrait Smooth Skin Tone (Astia)" | "F3/Studio Portrait Ex")
+		echo "Astia"
+		;;
+	"F2/Fujichrome (Velvia)" | "F4/Velvia")
+		echo "Velvia"
+		;;
+	"Bleach Bypass")
+		echo "Enterna Bleach Bypass"
+		;;
+	"Reala ACE")
+		echo "Reala Ace"
+		;;
+	"None (B&W)")
+		echo "Monochrome"
+		;;
+	"B&W Sepia")
+		echo "Sepia"
+		;;
+	"B&W Red Filter" | "B&W Yellow Filter" | "B&W Green Filter")
+		echo "${film_sim//B&W/Monochrome}"
+		;;
+	*)
+		echo "$film_sim"
+		;;
+	esac
+}
+
+format_wb_fine_tune_scaled() {
 	get_value "WhiteBalanceFineTune" | awk '{printf "Red %+d, Blue %+d", $2/20, $4/20}'
 }
-ansi_invert='\e[1;7m'
-ansi_reset='\e[0m'
-box_width=60
-label_width=25
 
-function print_heading_line() {
+get_white_balance() {
+	local wb
+	wb="$(get_value "WhiteBalance")"
+	if [[ $wb == "Kelvin" ]]; then
+		echo "$(get_value "ColorTemperature")K"
+	else
+		echo "$wb"
+	fi
+}
+
+get_dynamic_range() {
+	local dr_type
+	dr_type="$(get_value "DynamicRangeSetting")"
+	if [[ $dr_type == "Manual" ]]; then
+		echo "DR$(get_value "DevelopmentDynamicRange")"
+	else
+		echo "$dr_type"
+	fi
+}
+
+get_grain_effect() {
+	local grain_effect
+	grain_effect="$(get_value "GrainEffectRoughness")"
+	if [[ $grain_effect == "Off" ]]; then
+		echo "Off"
+	else
+		echo "$grain_effect, $(get_value "GrainEffectSize")"
+	fi
+}
+
+print_heading_line() {
 	local heading="$1"
 	local file_name="$2"
 	local used_width=$((${#heading} + ${#file_name} + 6))
@@ -55,22 +129,22 @@ function print_heading_line() {
 	border_segment=$(printf "%*s" $remaining_width)
 	printf "╔${ansi_invert}%s %s %s${ansi_reset}╗\n" "$heading" "$border_segment" "$file_name"
 }
-function print_section_divider() {
+print_section_divider() {
 	local fill_width=$((box_width - 2))
 	printf "╠%s╣\n" "$(printf "%*s" $fill_width | tr ' ' '═')"
 }
 
-function print_section_heading_line() {
+print_section_heading_line() {
 	local section_heading="$1"
 	local heading_len=${#section_heading}
 	local padding_needed=$((box_width - 5 - heading_len))
 	printf "║ ${ansi_invert} %s ${ansi_reset}%*s║\n" "$section_heading" "$padding_needed" ""
 }
 
-function print_data_line() {
+print_data_line() {
 	local label="$1"
 	local raw_value="$2"
-	local max_value_len=$((box_width - 5 - label_width))
+	local max_value_len=$((box_width - 5 - box_label_width))
 	local value="$raw_value"
 	if [ ${#raw_value} -gt $max_value_len ] && [ $max_value_len -ge 1 ]; then
 		local truncate_len=$((max_value_len - 1))
@@ -80,14 +154,14 @@ function print_data_line() {
 		value="${raw_value:0:$truncate_len}…"
 	fi
 	local value_len=${#value}
-	local padding_needed=$((box_width - 4 - label_width - value_len))
+	local padding_needed=$((box_width - 4 - box_label_width - value_len))
 	if [ $padding_needed -lt 0 ]; then
 		padding_needed=0
 	fi
-	printf "║ %-${label_width}s %s%*s║\n" "$label" "$value" $padding_needed ""
+	printf "║ %-${box_label_width}s %s%*s║\n" "$label" "$value" $padding_needed ""
 }
 
-function print_end_line() {
+print_end_line() {
 	local fill_width=$((box_width - 2))
 	printf "╚%s╝\n" "$(printf "%*s" $fill_width | tr ' ' '═')"
 }
@@ -95,6 +169,13 @@ function print_end_line() {
 camera_make=$(get_value "Make")
 camera_model_full=$(get_value "Model")
 camera_model_short=$(echo "$camera_model_full" | sed "s/$camera_make//i" | xargs)
+
+color="$(get_value "Saturation")"
+
+is_bw=false
+if [[ $color =~ .*(Acros|B\&W).* ]]; then
+	is_bw=true
+fi
 
 printf "\n"
 print_heading_line "Fujifilm Recipe Card" "$file_name"
@@ -113,15 +194,16 @@ print_data_line "Exposure Comp." "$(get_value "ExposureCompensation")"
 print_data_line "Metering Mode" "$(get_value "MeteringMode")"
 print_section_divider
 print_section_heading_line "Recipe"
-print_data_line "Film Simulation" "$(get_value "FilmMode")"
-print_data_line "White Balance" "$(get_value "WhiteBalance")"
+print_data_line "Film Simulation" "$(get_film_sim)"
+print_data_line "White Balance" "$(get_white_balance)"
 print_data_line "WB Shift" "$(format_wb_fine_tune_scaled)"
-print_data_line "Color" "$(get_value "Saturation")"
+if ! $is_bw; then
+	print_data_line "Color" "$color"
+fi
 print_data_line "Highlight" "$(get_value "HighlightTone")"
 print_data_line "Shadow" "$(get_value "ShadowTone")"
-print_data_line "Dynamic Range" "$(get_value "DynamicRange")"
-print_data_line "Grain Roughness" "$(get_value "GrainEffectRoughness")"
-print_data_line "Grain Size" "$(get_value "GrainEffectSize")"
+print_data_line "Dynamic Range" "$(get_dynamic_range)"
+print_data_line "Grain Effect" "$(get_grain_effect)"
 print_data_line "Color Chrome Effect" "$(get_value "ColorChromeEffect")"
 print_data_line "Color Chrome FX Blue" "$(get_value "ColorChromeFXBlue")"
 print_data_line "Sharpness" "$(get_value "Sharpness")"
